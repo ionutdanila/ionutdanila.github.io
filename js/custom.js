f90cff5d5536d1c695f0482f43d0d24a2e4a4dca
Ionuț Dănilă <hello@ionutdanila.com>
2020-05-18 07:18:29 +0000

    Update Work Projects

--- js/custom.js
+++ js/custom.js
@@ -92,6 +92,7 @@
 	// Projects accordion
 	$(function() {
 		$(".workProjects" ).accordion({
+			icons: { "header": "ui-icon-plus", "activeHeader": "ui-icon-minus" },
 		  collapsible: true,
 		  heightStyle: "content",
 		  active: false
